160ab5a1d467564f57ab5c8b6ba6b30beace06fe
jsilver01 <kje2066@naver.com>
2023-07-23 23:15:22 +0900

    add activity splash.xml, fragment_settings.xml, 아이콘들 수정

---
+++ couphone/app/src/main/java/com/kuit/couphone/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.kuit.couphone
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.couphone.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        loadSlash()
+    }
+
+    fun loadSlash() {
+        var handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
